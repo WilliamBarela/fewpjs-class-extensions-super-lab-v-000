94c9f8db77f704dfe11380b74d623f6dc395d51c
William Barela <william.barela.dev@gmail.com>
2020-02-24 07:54:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your classes here
+
+class Tree {
+  constructor(spcies){
+    this._species = species;
+  }
+  
+  static definition(){
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
+  }
+}
+
+class Deciduous{
+  constructor(species, name){
+    super(species);
+    this._name = name;
+  }
+  
+  static definition(){
+    return `${ super.definition() } Deciduos trees shed their leaves annually.`
+  }
+}
